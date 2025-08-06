844ffa0952b77139004d3c9fcae5a6b41eb37a68
Om’s Mac-Mini <contact@omshejul.com>
2025-08-06 11:19:05 +0530

    Add cookie configuration for NextAuth handler
    - Enhanced NextAuth configuration by adding secure cookie settings for CSRF token and PKCE code verifier.
    - Updated cookie options to ensure better security and compatibility with cross-site requests.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,28 @@ import NextAuth from "next-auth";
 
 export const runtime = "nodejs";
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth({
+  ...authOptions,
+  cookies: {
+    csrfToken: {
+      name: "next-auth.csrf-token",
+      options: {
+        httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: true,
+      },
+    },
+    pkceCodeVerifier: {
+      name: "next-auth.pkce.code_verifier", 
+      options: {
+        httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: true,
+      },
+    },
+  },
+});
 
 export { handler as GET, handler as POST };
